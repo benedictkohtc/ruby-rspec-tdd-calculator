8a3b148c920376c060b65356741a16a7c76a9e52
danialhr <danialharis@gmail.com>
2017-02-01 13:46:02 +0800

    add test for 1

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -7,10 +7,15 @@ describe Calculator do
   end
 
   describe 'Initialization' do
-    it 'is an instance of the MailBox class' do
+    it 'is an instance of the Calculator class' do
       expect(@my_calculator).to be_instance_of(Calculator)
     end
+  end
 
+    describe 'Inital Return' do
+      it 'should return 1' do
+        expect(@my_calculator.output).to eq(1)
+      end
   end
   # Tests go here
 end
