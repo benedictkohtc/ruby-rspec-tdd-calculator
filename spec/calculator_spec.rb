17b2b5d2d2d5456cadb29819122006af3e4749f2
danialhr <danialharis@gmail.com>
2017-02-01 13:21:38 +0800

    test 1 condition set,*Initialize: my_calculator = Calculator.new(1)

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -3,8 +3,14 @@ require_relative '../lib/item'
 
 describe Calculator do
   before(:context) do
-    # initialize calculator
+    @my_calculator = Calculator.new(1)
   end
 
+  describe 'Initialization' do
+    it 'is an instance of the MailBox class' do
+      expect(@my_calculator).to be_instance_of(Calculator)
+    end
+
+  end
   # Tests go here
 end
